96b53bb08a5f033251498ffc646c8351e2ba8514
Benjamn <bfreeman@lcs.on.ca>
2021-02-17 00:51:13 -0500

    Vertical padding constent

--- ProgressMeter/ContentView.swift
+++ ProgressMeter/ContentView.swift
@@ -15,18 +15,20 @@ struct ContentView: View {
     let meterWidth: CGFloat = 100
     // Thickness of meter's border
     let borderWidth: CGFloat = 2
+    // Padding above and below progress meter
+    let verticalPadding: CGFloat = 44
     var body: some View {
         VStack {
             ZStack {
                 // "Fill" for progress meter; stationary
                 Rectangle()
-                    .frame(width: meterWidth, height: 548 - 44, alignment: .center)
+                    .frame(width: meterWidth, height: 548 - verticalPadding, alignment: .center)
 
                 // Will slide up
                 Rectangle()
                     .fill(Color.primary)
                     .colorInvert()
-                    .frame(width: meterWidth, height: 548 - 44, alignment: .center)
+                    .frame(width: meterWidth, height: 548 - verticalPadding, alignment: .center)
                     .offset(progressMeterOffset)
                     .onAppear(perform: {
                         withAnimation(Animation.easeIn(duration: 4.0)) {
@@ -39,7 +41,7 @@ struct ContentView: View {
                 // This means the rectangle sliding up will pass beneath this view
                 Rectangle()
                     .fill(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
-                    .frame(width: meterWidth + borderWidth, height: 548 - 44 + borderWidth, alignment: .center)
+                    .frame(width: meterWidth + borderWidth, height: 548 - verticalPadding + borderWidth, alignment: .center)
                     .overlay(
                         Rectangle()
                             .stroke(Color.primary, lineWidth: 2)
