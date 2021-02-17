720830c587563350c022eabb4f45650b9cb1beda
Benjamn <bfreeman@lcs.on.ca>
2021-02-16 23:11:03 -0500

    Initial Commit

---
+++ ProgressMeter/ProgressMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressMeterApp.swift
+//  ProgressMeter
+//
+//  Created by Ben Freeman on 2021-02-16.
+//
+
+import SwiftUI
+
+@main
+struct ProgressMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
