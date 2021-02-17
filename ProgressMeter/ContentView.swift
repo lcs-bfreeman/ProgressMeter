e0aaa8c139915037ecd5f26d45999d26a72d9d81
Benjamn <bfreeman@lcs.on.ca>
2021-02-17 00:49:28 -0500

    New variables added for abstraction

--- ProgressMeter/ContentView.swift
+++ ProgressMeter/ContentView.swift
@@ -8,42 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     // For driving animation to reveal rectangle with progress meter fill
-        @State private var progressMeterOffset = CGSize.zero
+    @State private var progressMeterOffset = CGSize.zero
+    // Width of the meter
+    let meterWidth: CGFloat = 100
+    // Thickness of meter's border
+    let borderWidth: CGFloat = 2
     var body: some View {
         VStack {
-                ZStack {
-                    // "Fill" for progress meter; stationary
-                    Rectangle()
-                        .frame(width: 100, height: 548 - 44, alignment: .center)
-                       
-                    // Sits above the rectangle that slides up (in the z-axis)
-                    // This means the rectangle sliding up will pass beneath this view
-                    Rectangle()
-//                        .fill(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
-                        .frame(width: 100 + 2, height: 548 - 44 + 2, alignment: .center)
-                        .overlay(
-                            Rectangle()
-                                .stroke(Color.primary, lineWidth: 2)
-                        )
-                    
-                    // Will slide up
-                    Rectangle()
-                        .fill(Color.primary)
-                        .colorInvert()
-                        .frame(width: 100, height: 548 - 44, alignment: .center)
-                        .offset(progressMeterOffset)
-                        .onAppear(perform: {
-                            withAnimation(Animation.easeIn(duration: 4.0)) {
-                                // Offset is moves the opaque rectangle up
-                                progressMeterOffset = CGSize(width: 0, height: -1 * (548 - 44))
-                            }
-                        })
+            ZStack {
+                // "Fill" for progress meter; stationary
+                Rectangle()
+                    .frame(width: meterWidth, height: 548 - 44, alignment: .center)
 
-                }
+                // Will slide up
+                Rectangle()
+                    .fill(Color.primary)
+                    .colorInvert()
+                    .frame(width: meterWidth, height: 548 - 44, alignment: .center)
+                    .offset(progressMeterOffset)
+                    .onAppear(perform: {
+                        withAnimation(Animation.easeIn(duration: 4.0)) {
+                            // Offset is moves the opaque rectangle up
+                            progressMeterOffset = CGSize(width: 0, height: -1 * (548 - 44))
+                        }
+                    })
+
+                // Sits above the rectangle that slides up (in the z-axis)
+                // This means the rectangle sliding up will pass beneath this view
+                Rectangle()
+                    .fill(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
+                    .frame(width: meterWidth + borderWidth, height: 548 - 44 + borderWidth, alignment: .center)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color.primary, lineWidth: 2)
+                    )
+
             }
-    .padding()
-        
+
+        }
+
     }
 }
 
