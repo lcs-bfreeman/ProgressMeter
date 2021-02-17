cbcc310aeb41852666e03eaaab27990a7fcaf64c
Benjamn <bfreeman@lcs.on.ca>
2021-02-16 23:18:03 -0500

    Added two rectangles. one as outline and one as fill

--- ProgressMeter/ContentView.swift
+++ ProgressMeter/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            // "Fill" for progress meter; stationary
+            Rectangle()
+                .frame(width: 100, height: 548 - 44, alignment: .center)
+            // Will slide up
+            Rectangle()
+                .frame(width: 100, height: 548 - 44, alignment: .center)
+        }
+    .padding()
+        
     }
 }
 
