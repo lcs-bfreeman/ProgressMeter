fdff43be6fa4d0dcb3021eb5a5d50b9064e3f889
Benjamn <bfreeman@lcs.on.ca>
2021-02-17 00:46:09 -0500

    Three rectangles. outline added animation and sliders

--- ProgressMeter/ContentView.swift
+++ ProgressMeter/ContentView.swift
@@ -16,12 +16,29 @@ struct ContentView: View {
                     // "Fill" for progress meter; stationary
                     Rectangle()
                         .frame(width: 100, height: 548 - 44, alignment: .center)
+                       
+                    // Sits above the rectangle that slides up (in the z-axis)
+                    // This means the rectangle sliding up will pass beneath this view
+                    Rectangle()
+//                        .fill(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
+                        .frame(width: 100 + 2, height: 548 - 44 + 2, alignment: .center)
+                        .overlay(
+                            Rectangle()
+                                .stroke(Color.primary, lineWidth: 2)
+                        )
+                    
                     // Will slide up
                     Rectangle()
                         .fill(Color.primary)
                         .colorInvert()
                         .frame(width: 100, height: 548 - 44, alignment: .center)
                         .offset(progressMeterOffset)
+                        .onAppear(perform: {
+                            withAnimation(Animation.easeIn(duration: 4.0)) {
+                                // Offset is moves the opaque rectangle up
+                                progressMeterOffset = CGSize(width: 0, height: -1 * (548 - 44))
+                            }
+                        })
 
                 }
             }
